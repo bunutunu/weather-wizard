31f9662d4d435e4f335d23a845a70dc121ae0e9f
Hoang Quoc Huy <liechtiigbrettschneideig876@hotmail.com>
2025-04-10 08:47:47 +0700

    Create WeatherCard.vue

---
+++ WeatherCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <h2>{{ data.name }}</h2>
+    <img :src="iconUrl" :alt="data.weather[0].description" />
+    <p>{{ Math.round(data.main.temp) }}°{{ units === 'metric' ? 'C' : 'F' }}</p>
+    <p>{{ data.weather[0].main }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps(['data', 'units']);
+const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+</script>
+
+<style scoped>
+.card {
+  text-align: center;
+  margin-top: 1rem;
+}
+</style>
