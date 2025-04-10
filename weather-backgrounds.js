c1ad338b002850a9dd7e7c939927cb6f4fc4561f
Hoang Quoc Huy <liechtiigbrettschneideig876@hotmail.com>
2025-04-10 08:47:15 +0700

    Create weather-backgrounds.js

---
+++ weather-backgrounds.js
@@ -0,0 +1,9 @@
+export default {
+  Clear: 'sunny.jpg',
+  Clouds: 'cloudy.jpg',
+  Rain: 'rainy.jpg',
+  Snow: 'snow.jpg',
+  Thunderstorm: 'storm.jpg',
+  Drizzle: 'drizzle.jpg',
+  Mist: 'mist.jpg',
+};
